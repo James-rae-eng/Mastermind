ec059e61acc94c888b3e7902f009aabffb34ad17
James Rae <jrae151515@yahoo.co.uk>
2022-03-24 18:58:13 +0000

    Initial commit

---
+++ mastermind.rb
@@ -0,0 +1,27 @@
+class Maker
+    
+    def initialize
+        
+    end
+
+end
+
+class Breaker
+    
+    def initialize
+        
+    end
+
+end
+
+
+class Game 
+
+    def initialize
+        
+    end
+
+end  
+
+game = Game.new
+game.play
